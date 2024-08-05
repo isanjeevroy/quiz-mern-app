accbbe6c66fbc3e03bbe9e5e38c02417499ace37
Sanjeev Roy <sanjeev15062002@gmail.com>
2024-08-06 02:47:28 +0530

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
- server:{
-  proxy:{
-    '/api':'https://react-quiz-app-d2w3.onrender.com'
-  }
- },
+
   plugins: [react()],
 })
